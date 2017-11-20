fa363f80b10c8b3677e8e10d650ded61e243a04b
Alvaro Martinez <alvaromartinez@Alvaros-MBP-2.home>
2017-11-19 20:32:02 -0500

    featured venues static

--- events/src/app/app.module.ts
+++ events/src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatDatepickerModule, MatFormFieldModule, MatNativeDateModule } from '@angular/material';
-
+import {MatMenuModule} from '@angular/material';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeComponent } from './home/home.component';
+import { SearchPanelComponent } from './search-panel/search-panel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    HomeComponent
+    HomeComponent,
+    SearchPanelComponent
   ],
   imports: [
     BrowserModule,
     MatDatepickerModule,
     MatFormFieldModule,
-    MatNativeDateModule
+    MatNativeDateModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
