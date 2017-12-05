32e3a8b1e0ea511edd45bb5cd473b78c7d16f15c
Alvaro Martinez <alvaromartinez@Alvaros-MBP-2.home>
2017-12-05 16:49:41 -0500

    added server

---
+++ events/server/app.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const authRoutes = require('./routes/auth-routes');
+
+const app = express();
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port:8889, 
+    user: 'root',
+    password: 'root',
+    database: 'sit_parker'
+});
+
+connection.connect(function(err){
+    if(!err){
+        console.log("Database is connected ");
+    }else {
+        console.log(err);
+        console.log("Error connecting database");
+    }
+
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : false}));
+
+// Point static path to dist
+app.use(express.static(path.join(__dirname, '../dist')));
+
+
+
+app.use('/auth', authRoutes);
+
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+  });
+
+
+app.post('api/test', function () {
+    //let groceryServiceObj = new groceryService(req, res)
+    //groceryServiceObj.addGrocery()
+     res.send("reached end point");
+  });
+
+
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, () => console.log(`API running on localhost:${port}`));
