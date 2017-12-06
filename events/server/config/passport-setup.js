bc850c41a9c806bbc4b4344fd6a040fe5020fe9d
Alvaro Martinez <alvaromartinez@Alvaros-MBP-2.home>
2017-12-06 11:54:23 -0500

    set up google keys

--- events/server/config/passport-setup.js
+++ events/server/config/passport-setup.js
@@ -1,7 +1,11 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauh20');
-
+const keys = require('./keys');
 
 passport.use(new GoogleStrategy({
     //options for google 
-}));
+    clientID: keys.google.clientID,
+    clientSecret: keys.google.clientSecret 
+}),() => {
+
+});
